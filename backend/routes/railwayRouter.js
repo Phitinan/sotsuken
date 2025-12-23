16b97d7abd92cbacc787f71c003c8dca62250c36
Phitinan <Phitinan.Akrasuwanasri@metropolia.fi>
2025-12-23 16:06:26 +0200

    moved railway data to backend.

--- backend/routes/railwayRouter.js
+++ backend/routes/railwayRouter.js
@@ -1,5 +1,8 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import express from "express";
+
+const router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
